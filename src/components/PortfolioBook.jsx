ddd27c842de0b87966ee5da0ab1134deca769ef8
Carl Schmidt <klabelkholosh@gmail.com>
2025-02-11 09:46:57 +0200

    feat: fixed width issue for text on mainarea

--- src/components/PortfolioBook.jsx
+++ src/components/PortfolioBook.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import github_icon from '../images/github-mark.svg';
 import web_icon from '../images/web_link.svg';
 
@@ -6,6 +6,32 @@ import web_icon from '../images/web_link.svg';
 export default function PortfolioBook({ bookBody }) {
   let [currentPage, setCurrentPage] = React.useState(1);
   let numPages = bookBody.pages.length + 2; // adding 2 pages for the front cover + inner left cover
+  // let [extraClasses, setExtraClasses] = React.useState();
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.keyCode === 37) {
+        // left arrow press
+        handleLeftArrowPress();
+      } else if (event.keyCode === 39) {
+        // right arrow press
+        handleRightArrowPress();
+      }
+    };
+
+    const handleLeftArrowPress = () => {
+      console.log('left!');
+    };
+
+    const handleRightArrowPress = () => {
+      console.log('right!');
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
 
   const toggleClass = (e, toggleClassName) => {
     if (e.className) {
@@ -17,21 +43,36 @@ export default function PortfolioBook({ bookBody }) {
     }
   };
 
+  // handle page/arrow clicks to move pages of the book
   const movePage = (e, page, pageLimit) => {
     let eT = e.target;
 
     // only allow clicks up until the last page!
     if (page < pageLimit) {
       if (eT.className.substring(0, 5) === 'page ') {
+        // clicked the right-hand side page
         if (page === currentPage) {
           setCurrentPage(() => (currentPage += 2));
           toggleClass(eT, 'left-side');
           toggleClass(eT.nextElementSibling, 'left-side');
+          // clicked the left-hand side page
         } else if (page === currentPage - 1) {
           setCurrentPage(() => (currentPage -= 2));
           toggleClass(eT, 'left-side');
           toggleClass(eT.previousElementSibling, 'left-side');
         }
+      }
+    }
+  };
+
+  const movePage2 = (page, pageLimit) => {
+    if (page < pageLimit) {
+      // clicked the right-hand side page
+      if (page === currentPage) {
+        setCurrentPage(() => (currentPage += 2));
+        // clicked the left-hand side page
+      } else if (page === currentPage - 1) {
+        setCurrentPage(() => (currentPage -= 2));
       }
     }
   };
@@ -47,6 +88,7 @@ export default function PortfolioBook({ bookBody }) {
         <span
           className="page cover-front"
           onClick={(e) => movePage(e, 1, numPages)}
+          key={`pfb-sp-1`}
         >
           <h1>{bookBody.title}</h1>
         </span>
@@ -54,6 +96,7 @@ export default function PortfolioBook({ bookBody }) {
         <span
           className="page cover-front"
           onClick={(e) => movePage(e, 2, numPages)}
+          key={`pfb-sp-2`}
         ></span>
 
         {bookBody.pages.map((el, idx) => {
@@ -61,7 +104,7 @@ export default function PortfolioBook({ bookBody }) {
             <span
               className="page text-page"
               onClick={(e) => movePage(e, idx + 3, numPages)}
-              key={`pfb-sp-${idx}`}
+              key={`pfb-sp-${idx + 3}`}
             >
               <h1>{el.page_title}</h1>
               <p>{el.desc}</p>
