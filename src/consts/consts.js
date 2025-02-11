525e05808b5d87eeff88671f5f4205be315ea4e1
Carl Schmidt <klabelkholosh@gmail.com>
2025-02-11 15:10:53 +0200

    feat: removed firebase stuff

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -13,7 +13,7 @@ export const BOOK_TEXT = [
     type: 'basic',
     text: [
       `ABOUT ME`,
-      `My name is Ben Dunlop, and I'm a front end developer / UI person.`,
+      `My name is Ben Dunlop, and I'm a front-end developer / UI person.`,
       `I like React, JS, SVGs, icon design, and getting more into animations at the moment!`,
       `I do some back-end as well (Node.js, APIs, SQL admin, Linux & SCO/Unix admin).`,
       `My previous experience is PHP/PostgreSQL (and niche print-industry ERP software).`,
